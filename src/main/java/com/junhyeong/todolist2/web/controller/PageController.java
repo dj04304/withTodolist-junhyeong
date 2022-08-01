ca5b89508deb31176f0fd177133d28ffddaf7f13
dj04304 <dj043045@gmail.com>
2022-08-01 21:55:00 +0900

    view 디자인 완성 및 자바스크립트로 data 받아오기

---
+++ src/main/java/com/junhyeong/todolist2/web/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.junhyeong.todolist2.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todolist")
+public class PageController {
+	
+	@GetMapping("/index")
+	public String loadIndex() {
+		return "index";
+	}
+
+}
